42d9fce761e641dd6e33319dd0a811513597fbf4
Antoine07 <antoine.lucsko@gmail.com>
2021-06-10 15:24:55 +0200

    e3 e4

--- cours/Exercices/Exercices_03/exercices.sql
+++ cours/Exercices/Exercices_03/exercices.sql
@@ -18,6 +18,33 @@ ADD numStreet INT UNSIGNED ;
 ALTER TABLE compagnies
 DROP numStreet ;    
 
-
 ALTER TABLE compagnies
 ADD numStreet INT UNSIGNED AFTER `name` ;
+
+
+-- pilots
+CREATE TABLE `pilots` (
+    `certificate` VARCHAR(6),
+    `numFlying` DECIMAL(7,1),
+    `compagny` CHAR(4),
+    `name` VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_pilots PRIMARY KEY (`certificate`)
+) ENGINE=InnoDB ;
+
+-- add constraint FOREIN KEY
+
+ALTER TABLE pilots ADD CONSTRAINT fk_pilots_compagny FOREIGN KEY (compagny) REFERENCES compagnies(`comp`);
+
+
+-- add constraint unique
+ALTER TABLE pilots ADD CONSTRAINT un_name UNIQUE (`name`) ;
+
+-- drop constraint 
+ALTER TABLE pilots DROP INDEX `un_name`;
+
+
+-- show all constraint
+
+SELECT COLUMN_NAME, CONSTRAINT_NAME, REFERENCED_COLUMN_NAME, REFERENCED_TABLE_NAME
+FROM information_schema.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'pilots';
