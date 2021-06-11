89b18ce94f36ef51c81635b77625f6bd80e9968f
Antoine07 <antoine.lucsko@gmail.com>
2021-06-11 10:56:36 +0200

    exos

--- cours/Exercices/Exercices_05/exercices.sql
+++ cours/Exercices/Exercices_05/exercices.sql
@@ -57,5 +57,87 @@ SELECT `name`, compagny FROM pilots WHERE compagny LIKE '%i_'  ;
 
 /*
 Exercice supplémentaire avec une Regex ceci permet d'extraire des sous-chaînes
-Récupérez tous les noms des pilotes dont le nom de leur compagnie se termine par I suivi de 2 caractères exactement
+Récupérez tous les noms des pilotes dont le nom de leur compagnie se termine par R suivi de 2 caractères exactement
 */
+
+
+SELECT name, compagny FROM pilots WHERE compagny REGEXP '.*r.{2}$';
+
+-- vous pouvez tester les Regex directement en console 
+
+SELECT "ANTOINE" REGEXP '^A.*E$' ;
+
+SELECT "ANTOINE" REGEXP '^A.*X$' ;
+
+/*
+
+^  : commence par, ^A : commence par A
+
+*/
+
+SELECT "ANTOINE" REGEXP '^A' ;
+
+/*
+
+$  : se termine par,  E$ : se termine par un E
+
+*/
+
+SELECT "ANTOINE" REGEXP 'E$' ;
+
+/*
+^  : commence par, ^A : commence par A
+
+$  : se termine par,  E$ : se termine par un E
+
+*/
+
+SELECT "ANTOINE" REGEXP '^E$' ; 
+
+-- match avec E uniquement
+
+/*
+
+Quantificateur 
+* : 0, N
+{1,}  1 à N
+{, 2}  de 0 à 2
+{2} deux caractères exactements
+*/
+
+/*
+
+. : n'importe quel caractère
+[a-z] tous les caractères compris entre a et z
+[0-1] tous les caractères compris entre a et z
+[aZ-zZ0-1] tous les caractères alphanumérique
+
+*/
+
+
+-- Ajoutez une colonne Bonus
+
+ALTER TABLE pilots 
+ADD bonus DECIMAL(5,1) AFTER name;
+
+UPDATE pilots
+SET bonus = 1000
+WHere certificate IN ('ct-1', 'ct-11', 'ct-12');
+
+UPDATE pilots
+SET bonus = 2000
+WHere certificate IN ('ct-56');
+
+UPDATE pilots
+SET bonus = 500
+WHere certificate NOT IN ('ct-1', 'ct-11', 'ct-12', 'ct-56'); -- pensez au complémentaire
+
+-- en une requete avec case when
+
+UPDATE `pilots`
+    SET `bonus` =
+        (CASE
+            WHEN `certificate` IN ('ct-1', 'ct-11', 'ct-12') THEN 1000
+            WHEN `certificate` IN ('ct-56') THEN 2000
+            ELSE 500
+        END);
